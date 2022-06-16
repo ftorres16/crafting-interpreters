0f804588f3525ad31e789808bcd57e91b6523abb
fabian <36959980+ftorres16@users.noreply.github.com>
2022-06-16 13:03:44 -0400

    hash tables

--- clox/src/vm.h
+++ clox/src/vm.h
@@ -2,6 +2,7 @@
 #define clox_vm_h
 
 #include "chunk.h"
+#include "table.h"
 #include "value.h"
 
 #define STACK_MAX 256
@@ -11,6 +12,7 @@ typedef struct {
   Value stack[STACK_MAX];
   uint8_t *ip;
   Value *stackTop;
+  Table strings;
   Obj *objects;
 } VM;
 
