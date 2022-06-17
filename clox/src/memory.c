607d8130a9c1b6d025db67a3387da9464bf5ac16
fabian <36959980+ftorres16@users.noreply.github.com>
2022-06-17 17:59:23 -0400

    classes and instances

--- clox/src/memory.c
+++ clox/src/memory.c
@@ -75,6 +75,12 @@ static void freeObject(Obj *object) {
     FREE(ObjFunction, object);
     break;
   }
+  case OBJ_INSTANCE: {
+    ObjInstance *instance = (ObjInstance *)object;
+    freeTable(&instance->fields);
+    FREE(ObjInstance, object);
+    break;
+  }
   case OBJ_NATIVE:
     FREE(ObjNative, object);
     break;
@@ -108,6 +114,11 @@ static void blackenObject(Obj *object) {
   printf("\n");
 #endif
   switch (object->type) {
+  case OBJ_CLASS: {
+    ObjClass *klass = (ObjClass *)object;
+    markObject((Obj *)klass->name);
+    break;
+  }
   case OBJ_CLOSURE: {
     ObjClosure *closure = (ObjClosure *)object;
     markObject((Obj *)closure->function);
@@ -120,6 +131,12 @@ static void blackenObject(Obj *object) {
     ObjFunction *function = (ObjFunction *)object;
     markObject((Obj *)function->name);
     markArray(&function->chunk.constants);
+    break;
+  }
+  case OBJ_INSTANCE: {
+    ObjInstance *instance = (ObjInstance *)object;
+    markObject((Obj *)instance->klass);
+    markTable(&instance->fields);
     break;
   }
   case OBJ_UPVALUE:
